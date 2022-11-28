c12fa1ef0b682068cb7c44422b8c6c4c2d5958da
mayxly <may.ly7303@gmail.com>
2022-11-28 09:47:34 -0500

    Initial commit with game.cc and game.h file.

---
+++ game.cc
@@ -0,0 +1,5 @@
+#include "game.h"
+using namespace std;
+
+Game::Game() : turn{turn}, p1score{p1score}, p2score{p2score} {}
+
---
+++ game.h
@@ -0,0 +1,13 @@
+#ifndef GAME_H
+#define GAME_H
+class Game {
+private:
+ int turn;
+ int p1score;
+ int p2score;
+
+public:
+ Game();
+};
+
+#endif
