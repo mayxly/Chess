58d3feff47626cbabb859761933a5fbf7930caee
Raj Saragadam <rishi.saragadam@gmail.com>
2022-11-28 23:17:33 -0500

    Updated .h file

--- display.h
+++ display.h
@@ -1,14 +1,18 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 #include "game.h"
+#include <string>
 
 class Display {
 private:
  Game *game;
- int test;
 
 public:
  Display(Game *game);
+ void printBoard(Board *b);
+ void printMsg(std::string msg);
+ void printWin(std::string msg);
+ ~Display();
 };
 
 #endif
